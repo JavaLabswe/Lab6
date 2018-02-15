1dc775c08fa9ba652c593f58886dc8eff9848909
Asus_laptop\Asus <Palmy_kiss8@hotmail.com>
2018-02-15 22:32:21 +0700

    ข้อ 6 เด้อออ

---
+++ src/SumRecursive.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SumRecursive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		System.out.print("input");
+		int n=in.nextInt();
+		System.out.print(""+Sum(n));
+	}
+	
+		public static int Sum(int n) {
+			if(n>1) {
+				return n+Sum(n-1);
+			}else {
+				return 1;
+			}
+
+		}
+	}
+
+
