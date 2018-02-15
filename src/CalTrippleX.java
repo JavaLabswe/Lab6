d8e7d6e7da3c0653f087f1dc50f939d8e28bf321
Asus_laptop\Asus <Palmy_kiss8@hotmail.com>
2018-02-15 21:16:46 +0700

    ข้อ1คะ

---
+++ src/CalTrippleX.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CalTrippleX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(trippleX(5));
+		System.out.println(trippleX(5.5));
+	}
+	public static int trippleX(int x) {
+		return x*x*x;
+	}
+	public static double trippleX(double x) {
+		return x*x*x;
+	}
+
+}
